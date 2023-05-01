865c5b1132bf4dd402ecff700e8f365e48f77182
Shamiul haque <shamiulhaque1218@gmail.com>
2023-05-02 01:47:18 +0600

    footer

--- chef_menu/src/componemt/Main.jsx
+++ chef_menu/src/componemt/Main.jsx
@@ -1,11 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import Casusel from './Casusel';
+import Footer from './Footer';
 
 const Main = () => {
     return (
         <div>
            <Casusel> </Casusel>
+           <Footer> </Footer>
         </div>
     );
 };
